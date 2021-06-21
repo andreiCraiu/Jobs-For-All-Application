da72ff03cbc00b175cdfaefe0e20548b00870601
andreiCraiu <64604622+andreiCraiu@users.noreply.github.com>
2021-06-21 21:53:00 +0300

    adding components
    adding login, logout, heading
    prepare the stab

--- Frontend/FinalProject/src/app/app.module.ts
+++ Frontend/FinalProject/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { HeaderComponent } from './component/header/header.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SigninComponent } from './component/authentication/signin/signin.component';
+import { RegisterComponent } from './component/authentication/register/register.component';
+import {MatCardModule} from '@angular/material/card';
+import { MainPageComponent } from './component/main-page/main-page.component';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    SigninComponent,
+    RegisterComponent,
+    MainPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
