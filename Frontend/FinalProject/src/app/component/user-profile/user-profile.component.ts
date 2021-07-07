7c22d2acf3c8f24fdda1c8e548fd3eb78a765f41
andreiCraiu <64604622+andreiCraiu@users.noreply.github.com>
2021-07-07 20:55:03 +0300

    Update user-profile.component.ts

--- Frontend/FinalProject/src/app/component/user-profile/user-profile.component.ts
+++ Frontend/FinalProject/src/app/component/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
-
+  editUserFormGroup!: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
